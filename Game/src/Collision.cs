569d47e28094ec08514e8f47b5a922ba5f47c400
luizblank <luizrosacardoso@gmail.com>
2024-01-17 11:43:32 -0300

    Added collision verify

--- Game/src/Collision.cs
+++ Game/src/Collision.cs
@@ -1,13 +1,45 @@
 using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
-public static class Collision {
-    public static List<Entity> Entities { get; set; }
+public static class Entities
+{
+    private static List<Entity> entities = new List<Entity>();
+
+    public static void Add(Entity entity)
+    {
+        entities.Add(entity);
+    }
+
+    public static List<Entity> Get()
+    {
+        return entities;
+    }
+
+    public static Entity Get(int index)
+    {
+        return entities[index];
+    }
 
     public static void VerifyCollision()
     {
-        foreach (var entity in Entities)
+        foreach (var entity in entities)
         {
-            entity.OnCollision(entity);
+            bool collision = false;
+            foreach (var allEntities in entities)
+            {
+                if (entity != allEntities)
+                {
+                    bool result = entity.Hitbox.VerifyCollision(entity, allEntities);
+
+                    if (result)
+                        collision = true;
+                }
+            }
+            if (collision)
+                entity.Hitbox.pen = Pens.Blue;
+            else
+                entity.Hitbox.pen = Pens.Red;
         }
     }
 }
